5280c9bdd40f130117720ec0cea030a3b4bc9c1b
Konstantin0 <maschkovc@yandex.ru>
2023-03-17 13:01:52 +0300

    задний фон для меню

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,13 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.navigation'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    overlay = document.querySelector('.overlay');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('navigation_active');
+        overlay.classList.toggle('overlay_active');
     });
 
     // menuItem.forEach(item => {
