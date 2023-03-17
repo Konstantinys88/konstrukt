d0ffa3410834fe09691c288ce41c11e2cb594cb6
Konstantin0 <maschkovc@yandex.ru>
2023-03-17 11:44:47 +0300

    выплывающее меню

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.navigation'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('navigation_active');
+    });
+
+    // menuItem.forEach(item => {
+    //     item.addEventListener('click', () => {
+    //         hamburger.classList.toggle('hamburger_active');
+    //         menu.classList.toggle('navigation__wrapper_active');
+    //     });
+    // });
+});
